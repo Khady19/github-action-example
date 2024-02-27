e0327fbecd84f7ca586cf4040d15475b31305b25
Khady19 <diawarak19@gmail.com>
2024-02-27 13:45:41 +0000

    github pdf

--- src/main/java/sn/isi/CIDCI/controller/MyController.java
+++ src/main/java/sn/isi/CIDCI/controller/MyController.java
@@ -1,13 +1,15 @@
-package Controller;
+package sn.isi.CIDCI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class MyController {
-    @GetMapping("/")
+    @GetMapping
     public String hello(){
-        return "Coucou tout le monde";
+        return "Coucou ngor";
 
     }
 }
